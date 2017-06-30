0d5356b880349e33d88728dfa1c9e9bffef77b55
elvis <mosoti17@gmail.com>
2017-06-30 16:12:16 +0300

    added bower files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,18 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var buildProduction = utilities.env.production;
+var browserSync = require('browser-sync').create();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
@@ -39,4 +51,45 @@ gulp.task("build", ['clean'], function(){
     gulp.start('jsBrowserify');
   }
 
+});
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+gulp.watch(['js/*.js'], ['jsBuild']);
+gulp.watch(['bower.json'], ['bowerBuild']);
+gulp.watch(['*.html'], ['htmlBuild']);
+gulp.watch(["scss/*.scss"], ['cssBuild']);
+});
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+ gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+ });
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
+});
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
--- js/github.js
+++ js/github.js
@@ -2,9 +2,10 @@ var apiKey = require('./../.env').apiKey;
 function Github(user) {
   $.get('https://api.github.com/users/'+user+'?access_token=' + apiKey).then(function(response){
   console.log(response);
+  console.log(response.name);
    console.log(response.avatar_url);
    console.log(response.public_repos);
-   console.log(response.repos_url.name);
+   console.log(response.repos_url);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
